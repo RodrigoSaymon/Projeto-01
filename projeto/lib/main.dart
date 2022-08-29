a93e0bb89a588d8aa677fe262e85e33a2c0f5ad3
RodrigoSaymon <rodrigotbass@gmail.com>
2022-08-28 23:21:27 -0300

    atualizar

--- projeto/lib/main.dart
+++ projeto/lib/main.dart
@@ -1,70 +1,67 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Rodrigo());
+  runApp(const DolceSonhoConfeitaria());
 }
 
-class Rodrigo extends StatefulWidget {
-  const Rodrigo({Key? key}) : super(key: key);
+class DolceSonhoConfeitaria extends StatefulWidget {
+  const DolceSonhoConfeitaria({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() {
-    return RodrigoState();
-  }
+  State<StatefulWidget> createState() => DolceSonhoState();
 }
 
-class RodrigoState extends State<Rodrigo> {
-  var pertuntas = ['Nome', 'Idade', 'Endereço', 'Telefone'];
-
-  var incrementoIndex = 0;
-
-  void perguntado() {
-    setState(() {
-      incrementoIndex++;
-    });
-  }
-
-  void desperguntado() {
-    setState(() {
-      incrementoIndex--;
-    });
-  }
-
+class DolceSonhoState extends State<DolceSonhoConfeitaria> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        backgroundColor: Color.fromRGBO(255, 228, 227, 1),
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Perguntador App'),
+          backgroundColor: const Color.fromRGBO(255, 228, 227, 1),
+          title: const Center(
+            child: Text(
+              'Dolce Sonho Confeitaria',
+              style: TextStyle(
+                  color: Color.fromRGBO(46, 24, 68, 1),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                  fontFamily: 'Halimun'),
+            ),
+          ),
         ),
-        body: Center(
+        body: Container(
+          color: Color.fromARGB(255, 255, 255, 255),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                pertuntas[incrementoIndex],
-                style:
-                    const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              Center(
+                child: Image.asset(
+                  'assets/image/brasão.png',
+                  width: 300,
+                  height: 300,
+                ),
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: desperguntado,
-                    child: const Icon(Icons.remove),
-                  ),
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  ElevatedButton(
-                    onPressed: perguntado,
-                    child: const Icon(Icons.add),
-                  ),
-                ],
+              ElevatedButton(
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.check,
+                      size: 50,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Catálogo',
+                      style: TextStyle(fontSize: 30),
+                    )
+                  ],
+                ),
               )
             ],
           ),
@@ -73,3 +70,81 @@ class RodrigoState extends State<Rodrigo> {
     );
   }
 }
+
+
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const Rodrigo());
+// }
+
+// class Rodrigo extends StatefulWidget {
+//   const Rodrigo({Key? key}) : super(key: key);
+
+//   @override
+//   State<StatefulWidget> createState() {
+//     return RodrigoState();
+//   }
+// }
+
+// class RodrigoState extends State<Rodrigo> {
+//   var pertuntas = ['Nome', 'Idade', 'Endereço', 'Telefone'];
+
+//   var incrementoIndex = 0;
+
+//   void perguntado() {
+//     setState(() {
+//       incrementoIndex++;
+//     });
+//   }
+
+//   void desperguntado() {
+//     setState(() {
+//       incrementoIndex--;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Perguntador App'),
+//         ),
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text(
+//                 pertuntas[incrementoIndex],
+//                 style:
+//                     const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+//               ),
+//               const SizedBox(
+//                 height: 30,
+//               ),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   ElevatedButton(
+//                     onPressed: desperguntado,
+//                     child: const Icon(Icons.remove),
+//                   ),
+//                   const SizedBox(
+//                     width: 30,
+//                   ),
+//                   ElevatedButton(
+//                     onPressed: perguntado,
+//                     child: const Icon(Icons.add),
+//                   ),
+//                 ],
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
